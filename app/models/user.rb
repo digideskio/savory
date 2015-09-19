63d535057bdf6040be91bd1c49bf04b4ce7963c6
Jackie Luo <jacquelineluo95@gmail.com>
2015-09-19 17:41:06 -0400

    Add new model for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User
   include Mongoid::Document
-  field :email, type: String
-  field :password_hash, type: String
+  include ActiveModel::SecurePassword
+
   field :first_name, type: String
   field :last_name, type: String
-  field :location, type: String
-  field :bio, type: String
+  field :email, type: String
+  field :password_digest, type: String
+
+  has_secure_password
 end
