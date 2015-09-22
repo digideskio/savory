7dbf2d0997e20562eda31b1ea085f6dc34edf35b
Jackie Luo <jacquelineluo95@gmail.com>
2015-09-22 14:25:44 -0400

    Add user authentication with Bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,35 @@
 class User
   include Mongoid::Document
-  include ActiveModel::SecurePassword
+  include BCrypt
+
+  attr_accessor :password
 
   field :first_name, type: String
   field :last_name, type: String
   field :email, type: String
-  field :password_digest, type: String
+  field :password_hash, :type => String
 
-  has_secure_password
+  before_save :encrypt_password
+
+  def self.authenticate(email, password)
+    if password_correct?(email, password)
+      true
+    else
+      false
+    end
+  end
+  
+  def self.password_correct?(user_email, password)
+    user = find_by_email user_email
+    return if user.nil?
+    user_pass = Password.new(user.password_hash)
+    user_pass == password
+  end
+
+  protected
+  
+    def encrypt_password
+      self.password_hash = Password.create(@password)
+    end
+
 end
